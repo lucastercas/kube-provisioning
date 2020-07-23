f54bd3794f58d0178070de06b0a7a10d86e6f31a
lucastercas <lucasmtercas@gmail.com>
2020-07-22 22:48:04 -0300

    fix(terraform): Fix typos on terraform main and outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,13 @@
-variable "do_token" {}
+variable "do_token" {
+}
 
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = var.do_token
 }
 
 resource "digitalocean_ssh_key" "default" {
   name       = "hyperion ssh key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "digitalocean_droplet" "loadbalancer" {
@@ -22,7 +23,7 @@ resource "digitalocean_droplet" "master" {
   count    = 3
   name     = "master-${count.index}"
   image    = "debian-10.3-x64"
-  size     = "s-1vcpu-1gb"
+  size     = "s-2vcpu-3gb"
   region   = "lon1"
   ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
 }
@@ -31,7 +32,7 @@ resource "digitalocean_droplet" "worker" {
   count    = 0
   name     = "worker-${count.index}"
   image    = "debian-10.3-x64"
-  size     = "s-1vcpu-1gb"
+  size     = "s-2vcpu-3gb"
   region   = "lon1"
   ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "loadbalancer-ipv4_addresses" {
-  value = ["${digitalocean_droplet.loadbalancer.*.ipv4-address}"]
+  value = ["${digitalocean_droplet.loadbalancer.*.ipv4_address}"]
 }
 
 output "master-ipv4_addresses" {
-  value = ["${digitalocean_droplet.master.*.ipv4-address}"]
+  value = ["${digitalocean_droplet.master.*.ipv4_address}"]
 }
 
 output "worker-ipv4_addresses" {
-  value = ["${digitalocean_droplet.worker.*.ipv4-address}"]
+  value = ["${digitalocean_droplet.worker.*.ipv4_address}"]
 }
