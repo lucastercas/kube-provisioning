0d2db01deac0c85512a138e6b27a12a120d078ef
lucastercas <lucasmtercas@gmail.com>
2020-07-28 18:06:39 -0300

    feat(initialize-cluster): Finish init of cluster

--- kubernetes/README.md
+++ kubernetes/README.md
@@ -2,6 +2,10 @@
 
 ## To-Do
 
+## Commands
+
+1. kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+
 ## References
 
 1. https://medium.com/@Oskarr3/setting-up-ingress-on-minikube-6ae825e98f82
--- terraform/digital_ocean/outputs.tf
+++ terraform/digital_ocean/outputs.tf
@@ -1,17 +1,20 @@
 output "loadbalancer_public" {
-  value = [
-    digitalocean_droplet.loadbalancer.*.ipv4_address
-  ]
+  value = [digitalocean_droplet.loadbalancer.*.ipv4_address]
+}
+output "loadbalancer_private" {
+  value = [digitalocean_droplet.loadbalancer.*.ipv4_address_private]
 }
 
 output "controlplane_public" {
-  value = [
-    digitalocean_droplet.master.*.ipv4_address
-  ]
+  value = [digitalocean_droplet.master.*.ipv4_address]
+}
+output "controlplane_private" {
+  value = [digitalocean_droplet.master.*.ipv4_address_private]
 }
 
+output "workers_private" {
+  value = [digitalocean_droplet.worker.*.ipv4_address]
+}
 output "workers_public" {
-  value = [
-    digitalocean_droplet.worker.*.ipv4_address
-  ]
+  value = [digitalocean_droplet.worker.*.ipv4_address_private]
 }
